12a700cfe18ac4b1673857e7eedca72d8afa33af
sdk-quadra <sadaka556@gmail.com>
2021-03-31 19:56:30 +0900

    herokuのdebug 419エラー

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -11,6 +11,7 @@
                 <div class="header__img-upload">
                     {!! Form::open(['url' => route('images.store'), 'files' => true, 'id' => 'header__img-upload-form']) !!}
 
+
                     <label for="img"><i class="far fa-plus-square"></i></label>
                     {!! Form::label('img', '', ['class' => 'header__img-upload-hidden-label']) !!}
 
